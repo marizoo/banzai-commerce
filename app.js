fdfc19c3fe4060a1d3ce78a97c84380d4b8538d5
marizoo <marizoo@meow.woof>
2022-02-06 15:58:39 +0700

    section: FEATURES: 100%

--- app.js
+++ app.js
@@ -6,3 +6,6 @@ menuItems.forEach((item, index) => {
         wrapper.style.transform = `translateX(${-100 * index}vw)`;
     });
 });
+
+// https://www.youtube.com/watch?v=b3Gqq_k-g24&t=73s
+// till minute 36
